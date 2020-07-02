04207f6ef7c1d3510689fe8c1bdaf768aa45bf2b
Kun Tang <binlaniua@gmail.com>
2020-07-02 10:57:44 +0800

    vue3支持template下多个root节点

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 // ESLint 检查 .vue 文件需要单独配置编辑器：
 // https://eslint.vuejs.org/user-guide/#editor-integrations
 module.exports = {
-  'extends': ['taro/vue3']
-}
+  extends: ["taro/vue3"],
+  rules: {
+    "vetur.validation.template": false
+  }
+};
